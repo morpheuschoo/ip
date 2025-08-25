c077271d742232860ca02e0adf627b33a88e40a9
morpheuschoo <morpheuschoo@gmail.com>
2025-08-26 02:00:03 +0800

    Added dates and times: Dates and time are now stored as LocalDateTime instead of String

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,19 +1,30 @@
 package tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    private String dueDate;
+    private LocalDateTime dueDate;
     
-    public Deadline(String desc, String dueDate) {
+    private static final DateTimeFormatter INPUT_FORMAT =
+            DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+
+    private static final DateTimeFormatter DISPLAY_FORMATTER = 
+            DateTimeFormatter.ofPattern("d MMMM yyyy, h:mma");
+
+    public Deadline(String desc, String dueDate) throws DateTimeParseException {
         super(desc);
-        this.dueDate = dueDate;
+        this.dueDate = LocalDateTime.parse(dueDate, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.dueDate + ")"; 
+        return "[D]" + super.toString() + " (by: " + this.dueDate.format(DISPLAY_FORMATTER) + ")"; 
     }
 
     public String getDueDate() {
-        return this.dueDate;
+        return this.dueDate.format(INPUT_FORMAT);
     }
 }
